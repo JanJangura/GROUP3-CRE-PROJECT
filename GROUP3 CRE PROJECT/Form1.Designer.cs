98a8217a8e4fd3fff6ffcc652533b362f4bde69b
JanJangura <111806379+JanJangura@users.noreply.github.com>
2022-12-08 00:20:35 -0500

    VR

--- GROUP3 CRE PROJECT/Form1.Designer.cs
+++ GROUP3 CRE PROJECT/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "txtContents";
+            this.textBox1.Location = new System.Drawing.Point(36, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(1045, 322);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.AccessibleName = "btnStart";
+            this.btnStart.Location = new System.Drawing.Point(308, 455);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(196, 72);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.AccessibleName = "btnStop";
+            this.btnStop.Location = new System.Drawing.Point(639, 455);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(196, 72);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1119, 573);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- GROUP3 CRE PROJECT/Form1.cs
+++ GROUP3 CRE PROJECT/Form1.cs
@@ -7,14 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
 
 namespace GROUP3_CRE_PROJECT
 {
     public partial class Form1 : Form
     {
+        // Form Declarations...
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder(); 
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        Grammar gr = new DictationGrammar();
+        Choices clist;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Start (Button_Click)
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;            
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.SetInputToDefaultAudioDevice();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;               
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+                    ss.SpeakAsync("Hello Alex");
+                    break;
+                case "how are you":
+                    ss.SpeakAsync("I am doing great Alex, how about you");
+                    break;
+                case "what is the current time":
+                    ss.SpeakAsync("current time is " + DateTime.Now.ToLongTimeString());
+                    break;
+                case "open chrome":
+                    Process.Start("chrome", "https://www.google.com/");
+                    break;
+                case "close":
+                    Application.Exit();
+                    break;
+            }
+            textBox1.Text += e.Result.Text.ToString() + Environment.NewLine;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sre.RecognizeAsyncStop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
         }
     }
 }
